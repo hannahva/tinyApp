037d693851ebac78ca12f4483c2fa21a816043ad
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-04 19:57:40 +0000

    first commit

---
+++ express_server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (request, response) => {
+  response.end("Hello!");
+});
+app.get("/hello", (request, response) => {
+  response.end("<html><body>Hello <b>World</b></body></html>\n");
+})
+
+app.get("/urls.json", (request, response) =>{
+  response.json(urlDatabase);
+});
+app.get("/urls", (request, response) => {
+  let templateVars = { urls: urlDatabase};
+  response.render("urls_index", templateVars);
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
