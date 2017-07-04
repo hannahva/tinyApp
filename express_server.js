15a1066322dd593f13343fa63186a686b3491d77
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-04 21:30:49 +0000

    added body-parser as dependency, created urls_new which has a post method, updated express_server.js to make new route using urls_new. post request made on existing /urls endpoint

--- express_server.js
+++ express_server.js
@@ -4,14 +4,20 @@ const PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+
 app.get("/", (request, response) => {
   response.end("Hello!");
 });
+
 app.get("/hello", (request, response) => {
   response.end("<html><body>Hello <b>World</b></body></html>\n");
 })
@@ -19,16 +25,27 @@ app.get("/hello", (request, response) => {
 app.get("/urls.json", (request, response) =>{
   response.json(urlDatabase);
 });
+
 app.get("/urls", (request, response) => {
   let templateVars = { urls: urlDatabase};
   response.render("urls_index", templateVars);
 });
+
 app.get("/urls/:id", (request, response) => {
   let templateVars = {
     shortUrl: request.params.id,
     urls: urlDatabase
   };
   response.render("urls_show", templateVars);
+});
+
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
+});
+
+app.post("/urls", (request, response) => {
+  console.log(request.body);
+  response.send("Ok");
 });
 
 app.listen(PORT, () => {
