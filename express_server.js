8eca85626dfc744cce0dbbaa015ad98c52f213ae
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-05 21:42:44 +0000

    required cookie-parser, added username login form to _header

--- express_server.js
+++ express_server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
-
+const cookieParser = require("cookie-parser");
 //template engine
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,6 +18,10 @@ const urlDatabase = {
 
 app.get("/", (request, response) => {
   response.end("Hello!");
+});
+
+app.post("/login", (request, response) => {
+
 });
 
 // has to be above other urls/... pages to not
