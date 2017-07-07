0758bb6fd330c78b3d65610080be3e9d1d8fe0e1
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-07 04:59:11 +0000

    went through and checked all functionality against project requirements. checked against eslint.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
+// default port 8080
+const PORT = process.env.PORT || 8080;
 
 //template engine
 app.set("view engine", "ejs");
@@ -15,79 +16,6 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 const bcrypt = require("bcrypt");
-
-//FUNCTIONS:
-
-//checks given id against userID in urlDatabase
-const urlsForUser = (id) => {
-  const output = {};
-  for (url in urlDatabase){
-      if(urlDatabase[url].userID === id){
-        output[url] = urlDatabase[url];
-      };
-  };
-  return output;
-}
-
-//gets id associated with given email
-const getIdByEmail = (email) => {
-  let id = undefined;
-  for (user in users){
-    if(users[user].email === email){
-      id = user;
-    };
-  };
-  return id;
-}
-
-//
-const getUsernameById = (userID) => {
-  let user = users[userID];
-  if (!user){
-    return;
-  };
-  return user.email;
-};
-
-
-//check if user email already exists
-const checkUserEmail = (givenEmail) => {
-  for(user in users) {
-    if (users[user].email === givenEmail){
-      return true;
-    };
-  };
-  return false;
-};
-
-//check if password matches password linked to that email
-const checkPassword = (givenEmail, givenPW) => {
-  if(bcrypt.compareSync(givenPW, users[getIdByEmail(givenEmail)].password)){
-      return true;
-    }
-  return false;
-}
-
-
-//checks for implicit/explicit protocols on input
-//and adds if missing
-const addHTTP = (givenURL) => {
-  let newURL = givenURL;
-  if(!/https?:\/\//.test(givenURL)) {
-    if(!/www\./.test(givenURL)){
-      newURL = `www.${newURL}`;
-    }
-    newURL = `https://${newURL}`;
-    };
-  return newURL;
-};
-
-//creates short id's for given urls
-const generateRandomString = () => {
-  return Math.random().toString(36).substr(2, 6);
-};
-
-generateRandomString();
 
 //GLOBAL VARIABLES
 
@@ -108,22 +36,109 @@ const users = {
     email: "user@example.com",
     password: bcrypt.hashSync("purple-monkey-dinosaur", 10)
   },
- "user2RandomID": {
+  "user2RandomID": {
     id: "user2RandomID",
     email: "user2@example.com",
     password: bcrypt.hashSync("dishwasher-funk", 10)
   }
 };
 
-//ROUTES
+//FUNCTIONS:
 
+//checks given id against userID in urlDatabase
+const urlsForUser = (id) => {
+  const output = {};
+  for (url in urlDatabase){
+    if(urlDatabase[url].userID === id){
+      output[url] = urlDatabase[url];
+    }
+  }
+  return output;
+};
+
+//gets id associated with given email
+const getIdByEmail = (email) => {
+  let id = undefined;
+  for (user in users){
+    if(users[user].email === email){
+      id = user;
+    }
+  }
+  return id;
+};
+
+//
+const getUsernameById = (userID) => {
+  let user = users[userID];
+  if (!user){
+    return;
+  }
+  return user.email;
+};
+
+
+//check if user email already exists
+const checkUserEmail = (givenEmail) => {
+  for(user in users) {
+    if (users[user].email === givenEmail){
+      return true;
+    }
+  }
+  return false;
+};
+
+//check if password matches password linked to that email
+const checkPassword = (givenEmail, givenPW) => {
+  if(bcrypt.compareSync(givenPW, users[getIdByEmail(givenEmail)].password)){
+    return true;
+  }
+  return false;
+};
+
+
+//checks for implicit/explicit protocols on input
+//and adds if missing
+const addHTTP = (givenURL) => {
+  let newURL = givenURL;
+  if(!/https?:\/\//.test(givenURL)) {
+    if(!/www\./.test(givenURL)){
+      newURL = `www.${newURL}`;
+    }
+    newURL = `https://${newURL}`;
+  }
+  return newURL;
+};
+
+//creates short id's for given urls
+const generateRandomString = () => {
+  return Math.random().toString(36).substr(2, 6);
+};
+
+generateRandomString();
+
+
+
+//ROUTES
+app.get("/", (request, response) => {
+  if(!request.session.user_id){
+    response.redirect("/login");
+    return;
+  }
+  response.redirect("/urls");
+
+});
 //
 app.get("/login", (request, response) => {
   let userEmail = getUsernameById(request.session.user_id);
   let templateVars = {
     userEmail: userEmail
+  };
+  if(!!request.session.user_id){
+    response.redirect("/urls");
+
+  } else {
+    response.render("login", templateVars);
   }
-  response.render("login", templateVars);
 });
 
 //checks if email and password entered and correct
@@ -141,16 +156,14 @@ app.post("/login", (request, response) => {
   } else {
     request.session.user_id = id;
     response.redirect("/urls");
-    return;
-  };
-  response.redirect("/urls");
+  }
 });
 
 //post request for LOGOUT process
 app.post("/logout", (request, response) => {
   request.session = null;
   response.redirect("/urls");
-})
+});
 
 // has to be above other urls/... pages to not
 //get treated as /:id or /:shortURL by LocalHost
@@ -162,9 +175,9 @@ app.get("/urls/new", (request, response) => {
   if (templateVars.userEmail === undefined){
     response.redirect("/login");
     return;
-  };
-  response.render("urls_new", templateVars); //form to submit link to shorten
-})
+  }
+  response.render("urls_new", templateVars);
+});
 
 app.get("/urls.json", (request, response) =>{
   response.json(urlDatabase);
@@ -178,19 +191,24 @@ app.get("/urls", (request, response) => {
     urls: urlsForUser(request.session.user_id),
     userEmail: userEmail
   };
-  response.render("urls_index", templateVars);
+  if(!request.session.user_id){
+    response.status(401).send("Please login to continue");
+    return;
+  } else {
+    response.render("urls_index", templateVars);
+  }
 });
 
 //post route that removes a URL resource
 //and redirects to modified /urls
 app.post("/urls/:id/delete", (request, response) => {
   if (urlDatabase[request.params.id].userID === request.session.user_id){
-     delete urlDatabase[request.params.id];
-     response.redirect("/urls");
+    delete urlDatabase[request.params.id];
+    response.redirect("/urls");
   } else {
-     response.status(400).send("You do not have permission to delete that URL.")
-  };
-})
+    response.status(400).send("You do not have permission to delete that URL.");
+  }
+});
 
 //specific to unique id, displays that id's
 //short and long URL
@@ -202,18 +220,23 @@ app.get("/urls/:id", (request, response) => {
     userEmail: getUsernameById(request.session.user_id),
     cookie: request.session.user_id
   };
-  response.render("urls_show", templateVars);
+  if(urlDatabase[request.params.id] === undefined){
+    response.status(404).send("No URL exists for this ID");
+    return;
+  } else {
+    response.render("urls_show", templateVars);
+  }
 });
 
 //resets where short url points (ie corresponding long url)
 app.post("/urls/:id", (request, response) => {
-    let longURL = addHTTP(request.body.longURL);
-    urlDatabase[request.params.id].longURL = longURL;
-    if (urlDatabase[request.params.id].userID === request.session.user_id){
-        response.redirect("/urls");
-    } else {
-      response.status(400).send("You do not have permission to edit that URL.")
-    };
+  let longURL = addHTTP(request.body.longURL);
+  urlDatabase[request.params.id].longURL = longURL;
+  if (urlDatabase[request.params.id].userID === request.session.user_id){
+    response.redirect("/urls");
+  } else {
+    response.status(400).send("You do not have permission to edit that URL.");
+  }
 });
 
 app.get("/hello", (request, response) => {
@@ -234,17 +257,26 @@ app.post("/urls", (request, response) => {
 
 //shortened URL will redirect to its corresponding longURL
 app.get("/u/:shortURL", (request, response) => {
-  let longURL = urlDatabase[request.params.shortURL].longURL;
-  response.redirect(longURL);
-})
+  if(!urlDatabase[request.params.shortURL]){
+    response.status(400).send("No URL exists for this ID");
+    return;
+  } else {
+    let longURL = urlDatabase[request.params.shortURL].longURL;
+    response.redirect(longURL);
+  }
+});
 
 //registration page with email and password fields
 app.get("/register", (request, response) => {
   let userEmail = getUsernameById(request.session.user_id);
   let templateVars = {
     userEmail: userEmail
+  };
+  if(!!request.session.user_id){
+    response.redirect("/urls");
+  } else {
+    response.render("register", templateVars);
   }
-  response.render("register", templateVars);
 });
 
 //adds new user to user object
@@ -255,17 +287,17 @@ app.post("/register", (request, response) => {
     response.status(400).send("Email already exists");
     return;
   } else if (request.body.email && request.body.password){
-  let newUser = {
-    id: generateRandomString(),
-    email: request.body.email,
-    password: bcrypt.hashSync(request.body.password, 10)
-  };
-  users[newUser.id] = newUser;
-  request.session.user_id = newUser.id;
-  response.redirect("/urls");
+    let newUser = {
+      id: generateRandomString(),
+      email: request.body.email,
+      password: bcrypt.hashSync(request.body.password, 10)
+    };
+    users[newUser.id] = newUser;
+    request.session.user_id = newUser.id;
+    response.redirect("/urls");
   } else {
-     response.status(400).send("Both Password and Email field must be filled out");
-  };
+    response.status(400).send("Both Password and Email field must be filled out");
+  }
 
 });
 
